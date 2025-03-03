9328d0cce91a80f05115325e9110b07b020de878
Maxime <maxime.esteves81200@gmail.com>
2025-03-03 22:50:44 +0100

    Update Footer.tsx

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,5 +1,5 @@
 export const Footer = () => {
     return (
-        <p className="text-center mt-10 p-12 dark:bg-gray-50">© 2025 Maxime ESTEVES, tous droits réservés.</p>
+        <p className="text-center mt-10 p-12 ">© 2025 Maxime ESTEVES, tous droits réservés.</p>
     )
 }

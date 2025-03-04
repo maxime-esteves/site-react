237a1af588dee5fb0d40959dcc04c0f87ae2aff2
Maxime <maxime.esteves81200@gmail.com>
2025-03-04 01:33:37 +0100

    Update Main.tsx

--- app/_components/Main.tsx
+++ app/_components/Main.tsx
@@ -12,25 +12,25 @@ export const Main = () => {
         <>
         <div className=" grid justify-center ml-24 max-lg:w-[72%] max-lg:ml-36 max-md:ml-0 max-xs:w-[250px] max-xs:px-[200px]" >
 
-        <div className=" flex justify-start max-md:mr-16">
+        <div className=" flex justify-start max-md:mr-16 max-xs:ml-24">
             <Header />
         </div>
-        <div className=" max-md:mr-16">
+        <div className=" max-md:mr-16 max-xs:ml-24">
             <Projets />
         </div>
-        <div className="flex col-auto gap-4 mt-4 max-md:grid max-md:justify-center max-md:mr-16">
+        <div className="flex col-auto gap-4 mt-4 max-md:grid max-md:justify-center max-md:mr-16 max-xs:ml-24">
             <Formation />
             <Experiences />
         </div>
-        <div className=" flex justify-start ">
+        <div className=" flex justify-start max-xs:ml-24">
           <Skills />
       
         </div >
-        <div className=" flex justify-start max-md:mr-16">
+        <div className=" flex justify-start max-md:mr-16 max-xs:ml-24">
 
            <Contact /> 
         </div>
-        <div className=" flex justify-center max-md:mr-16">
+        <div className=" flex justify-center max-md:mr-16 max-xs:ml-24">
 
            <Footer />
         </div>

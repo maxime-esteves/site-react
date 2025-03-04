aa2bb3c06dc664a78ecf984b4621636225723ea3
Maxime <maxime.esteves81200@gmail.com>
2025-03-04 01:44:47 +0100

    Update Header.tsx

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -17,7 +17,7 @@ const Button = () => {
   return (
     <button
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="w-16 h-8 flex items-center bg-gray-300 dark:bg-gray-300 rounded-full p-1 transition-all duration-300 fixed ml-96 max-lg:ml-36 max-lg:static max-md:static "
+      className="w-16 h-8 flex items-center bg-gray-300 dark:bg-gray-300 rounded-full p-1 transition-all duration-300 fixed max-md:mt-8 max-md:static max-xl:static max-2xl:static"
     >
       <div
         className={`w-6 h-6 flex items-center justify-center rounded-full bg-black dark:bg-white shadow-md transform transition-all duration-300 ${
@@ -38,17 +38,17 @@ export default Button;
 
 export const Header = () => {
   return (
-    <header className=" max-md:ml-0  ">
-      <nav className=" flex space-x-10 mt-10 mr-20 mb-24 items-center max-md:mr-0 max-md:items-center max-md:flex-col max-md:gap-2 max-md:justify-center max-md:space-x-0 max-xl:justify-center ">
+    <header className="ml-20  max-md:ml-0 " >
+      <nav className="flex space-x-10 mt-10 mr-20 mb-24 items-center max-md:mr-0 max-md:items-center max-md:flex-col max-md:gap-2 max-md:justify-center max-md:space-x-0 max-xl:justify-center ">
         <Image
-          className="w-14 rounded-full "
+          className="w-14 rounded-full ml-96 max-md:ml-0 max-md:items-center max-2xl:ml-36 max-xl:ml-0"
           src="/photoProfil.png"
           alt="Photo de profil"
           width={56}
           height={56}
         />
 
-        <div className="border-[1px] hover:opacity-50 rounded-3xl py-2 px-4 flex items-center gap-8 ">
+        <div className="border-[1px] hover:opacity-50 rounded-3xl py-2 px-4 flex items-center gap-8">
           <a
             href="#projets"
             className="text-white hover:opacity-50 dark:text-black"
@@ -63,7 +63,7 @@ export const Header = () => {
           </a>
         </div>
 
-        <div className="border-[1px] rounded-3xl py-2 px-4 flex items-center gap-8 " >
+        <div className="border-[1px] rounded-3xl py-2 px-4 flex items-center gap-8" >
           <a href="CV_Maxime_E.pdf" target="blank">
             <Image
               className="max-w-[25px] invert hover:opacity-50 dark:invert-0"
@@ -102,7 +102,7 @@ export const Header = () => {
           </a>
           <a href="https://www.tiktok.com/@maximo_delavego?lang=fr" target="blank">
             <Image
-              className="max-w-[25px] invert hover:opacity-50 dark:invert-0 "
+              className="max-w-[25px] invert hover:opacity-50 dark:invert-0"
               src="/tiktok.png"
               alt="logo avec lien tiktok"
               
@@ -111,21 +111,20 @@ export const Header = () => {
             />
           </a>
         </div>
-        <div className=" flex ">
-  <div className="max-md:mr-[140px] ">
-    <Button />
-  </div>
-</div>
+
+        <div className="flex items-center px-96 max-md:px-0 max-2xl:px-0">
+          <Button />
+        </div>
       </nav>
 
-      <div className=" mb-8 flex flex-row items-center gap-8 max-xs:w-[200px]">
-        <h1 className="text-5xl font-bold text-white dark:text-black max-xs:w-[250px]">
+      <div className="ml-96 mb-8 flex flex-row items-center gap-8 max-md:ml-0  max-2xl:ml-36 max-md:justify-center">
+        <h1 className="text-5xl font-bold text-white dark:text-black max-md:w-[360px]">
           Développeur front-end
         </h1>
       </div>
 
-      <div className=" w-[700px] max-md:w-[550px] max-xs:w-[250px]">
-        <p className="text-base text-white mb-24 dark:text-black ">
+      <div className="ml-96 w-[700px] max-md:ml-0 max-md:w-auto max-md:flex max-md:flex-rows max-md:justify-center max-2xl:ml-36">
+        <p className="text-base text-white mb-24 dark:text-black max-md:w-[360px]">
           👋 Bonjour, je suis <strong className="text-red-500">Maxime Esteves</strong>, en
           reconversion professionnelle, je recherche une entreprise pour valider ma formation
           de Développeur Web en alternance.
@@ -137,7 +136,7 @@ export const Header = () => {
         </p>
       </div>
 
-      <div className="relative max-w-2xl mb-48 " id="projets">
+      <div className="relative max-w-2xl ml-96 mb-48 max-md:ml-12  max-md:mb-24 max-2xl:ml-36" id="projets">
         <div className="pointer-events-none">
           <div className="absolute right-1/2 translate-x-1/2 -top-10 h-px w-[100%] bg-gray-400 opacity-20"></div>
           <div className="absolute -top-10 right-1/2 h-[2px] w-48 rounded-full bg-gradient-to-r from-red-500"></div>

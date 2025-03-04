0c3300cfbb9a9daa6f4adfe109554e33a3b4d0a0
Maxime <maxime.esteves81200@gmail.com>
2025-03-04 01:44:15 +0100

    Update Forscsmation.tsx

--- app/_components/Formation.tsx
+++ app/_components/Formation.tsx
@@ -1,82 +1,147 @@
-import { JSX } from "react";
+"use client";
 import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Sun, Moon } from "lucide-react";
 
+const Button = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-export const Formation = () => {
-  return <ExpandableDiv />;
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // Empêche les erreurs de rendu côté serveur
+
+  return (
+    <button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      className="w-16 h-8 flex items-center bg-gray-300 dark:bg-gray-300 rounded-full p-1 transition-all duration-300 fixed max-md:mt-8 max-md:static max-xl:static max-2xl:static"
+    >
+      <div
+        className={`w-6 h-6 flex items-center justify-center rounded-full bg-black dark:bg-white shadow-md transform transition-all duration-300 ${
+          theme === "dark" ? "translate-x-8" : "translate-x-0"
+        }`}
+      >
+        {theme === "dark" ? (
+          <Sun className="text-yellow-400 w-4 h-4" />
+        ) : (
+          <Moon className="text-red-500 w-4 h-4" />
+        )}
+      </div>
+    </button>
+  );
 };
 
-export default function ExpandableDiv(): JSX.Element {
-  const formationVisible = [
-    {
-      id: 1,
-      title: "Titre Pro Développement Web",
-      description:
-        "3 semaines en entreprise / 1 semaine en formation. (100% distanciel)",
-      link: "",
-      image: "diplome.svg",
-      date: "2025 - 2026"
-    },
-    {
-      id: 2,
-      title: "BTS FED option C",
-      description:
-        "Fluides, Énergies, Domotique option Bâtiment communicants (31)",
-      link: "",
-      image: "diplome.svg",
-      date: "2017 - 2015"
-    },
-      {
-        id: 3,
-        title: "BAC Pro Électrotechnique",
-        description:
-          'Mention assez bien. (81)',
-        link: "",
-        image: "diplome.svg",
-        date: "2013 - 2015"
-      },
-  ];
-  return (
-    <div className="flex flex-col items-end max-md:flex-row max-md:w-auto max-md:justify-center">
-      {/* Div avec transition */}
-      <div
-        className={`w-auto border-[1px] p-4 rounded-lg h-[346px] max-md:h-[380px] max-md:ml-6 max-md:mr-6`}
-      >
-        <h1 className="text-white text-1xl font-bold mb-6 dark:text-black ">Mes formations</h1>
-       {formationVisible.map((formationVisible) => (
-            <div
-              key={formationVisible.id}
-              className="flex items-center gap-8 mb-6"
-            >
-              {/* Lien avec l'image */}
-              <div>
-                
-                  <Image 
-                    src={formationVisible.image}
-                    alt={`Icône de ${formationVisible.title}`}
-                    width={50} 
-                      height={50}
-                    className="max-w-[35px] invert ml-2 rounded-lg hover:p-1 dark:invert-0 dark:hover:bg-white dark:hover:invert"
-                  />
-              
-              </div>
-  
-              {/* Texte descriptif */}
-              <div>
-                <h3 className="text-white text-base font-medium tracking-tight dark:text-black">
-                  {formationVisible.title}
-                </h3>
-                <p className="text-gray-500 text-sm w-[280px] max-md:w-[200px]">{formationVisible.description}</p>
-              </div>
-              <div>
-                <p className="text-gray-300 text-xs dark:text-black">{formationVisible.date}</p>
+export default Button;
 
-              </div>
-            </div>
-          ))}
-        
+export const Header = () => {
+  return (
+    <header className="ml-20  max-md:ml-0 " >
+      <nav className="flex space-x-10 mt-10 mr-20 mb-24 items-center max-md:mr-0 max-md:items-center max-md:flex-col max-md:gap-2 max-md:justify-center max-md:space-x-0 max-xl:justify-center ">
+        <Image
+          className="w-14 rounded-full ml-96 max-md:ml-0 max-md:items-center max-2xl:ml-36 max-xl:ml-0"
+          src="/photoProfil.png"
+          alt="Photo de profil"
+          width={56}
+          height={56}
+        />
+
+        <div className="border-[1px] hover:opacity-50 rounded-3xl py-2 px-4 flex items-center gap-8">
+          <a
+            href="#projets"
+            className="text-white hover:opacity-50 dark:text-black"
+          >
+            projets
+          </a>
+          <a href="#skills" className="text-white hover:opacity-50 dark:text-black">
+            skills
+          </a>
+          <a href="#contact" className="text-white hover:opacity-50 dark:text-black">
+            contact
+          </a>
+        </div>
+
+        <div className="border-[1px] rounded-3xl py-2 px-4 flex items-center gap-8" >
+          <a href="CV_Maxime_E.pdf" target="blank">
+            <Image
+              className="max-w-[25px] invert hover:opacity-50 dark:invert-0"
+              src="/cv.png"
+              alt="logo avec lien cv"
+              width={25}
+              height={25}
+            />
+          </a>
+          <a href="mailto:maxime.esteves81@orange.fr?subject=Contact&body=Bonjour,%0AJe%20vous%20veux%20dans%20mon%20équipe%20!%20☺️" target="blank">
+            <Image
+              className="max-w-[25px] invert hover:opacity-50 dark:invert-0"
+              src="/mail.png"
+              alt="logo avec lien mail"
+              width={25}
+              height={25}
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/maxime-esteves-7a12b3225" target="blank">
+            <Image
+              className="max-w-[25px] invert hover:opacity-50 dark:invert-0"
+              src="/linkedin.svg"
+              alt="logo avec lien linkedin"
+              width={25}
+              height={25}
+            />
+          </a>
+          <a href="https://www.twitch.tv/maximodelavego"  target="blank">
+            <Image
+              className="max-w-[25px] invert hover:opacity-50 dark:invert-0"
+              src="/twitch.svg"
+              alt="logo avec lien twitch"
+              width={25}
+              height={25}
+            />
+          </a>
+          <a href="https://www.tiktok.com/@maximo_delavego?lang=fr" target="blank">
+            <Image
+              className="max-w-[25px] invert hover:opacity-50 dark:invert-0"
+              src="/tiktok.png"
+              alt="logo avec lien tiktok"
+              
+              width={25}
+              height={25}
+            />
+          </a>
+        </div>
+
+        <div className="flex items-center px-96 max-md:px-0 max-2xl:px-0">
+          <Button />
+        </div>
+      </nav>
+
+      <div className="ml-96 mb-8 flex flex-row items-center gap-8 max-md:ml-0  max-2xl:ml-36 max-md:justify-center">
+        <h1 className="text-5xl font-bold text-white dark:text-black max-md:w-[360px]">
+          Développeur front-end
+        </h1>
       </div>
 
-    </div>
+      <div className="ml-96 w-[700px] max-md:ml-0 max-md:w-auto max-md:flex max-md:flex-rows max-md:justify-center max-2xl:ml-36">
+        <p className="text-base text-white mb-24 dark:text-black max-md:w-[360px]">
+          👋 Bonjour, je suis <strong className="text-red-500">Maxime Esteves</strong>, en
+          reconversion professionnelle, je recherche une entreprise pour valider ma formation
+          de Développeur Web en alternance.
+          <br />
+          🔄 Rythme d&apos;alternance : 3 semaines en entreprise / 1 semaine en formation.
+          <br />
+          <br /> Je suis autodidacte, curieux et envieux d&apos;apprentissage ! Découvre mon
+          parcours et mes projets ☺️
+        </p>
+      </div>
+
+      <div className="relative max-w-2xl ml-96 mb-48 max-md:ml-12  max-md:mb-24 max-2xl:ml-36" id="projets">
+        <div className="pointer-events-none">
+          <div className="absolute right-1/2 translate-x-1/2 -top-10 h-px w-[100%] bg-gray-400 opacity-20"></div>
+          <div className="absolute -top-10 right-1/2 h-[2px] w-48 rounded-full bg-gradient-to-r from-red-500"></div>
+        </div>
+      </div>
+    </header>
   );
-}
+};
